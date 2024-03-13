266cdea142cc0c257f5e12858bbef7dc00311da9
katya123123 <katyku221133@gmail.com>
2024-03-14 00:42:43 +1000

    init

--- td/static/js/main.js
+++ td/static/js/main.js
@@ -4,11 +4,11 @@ import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 // To allow for importing the .gltf file
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+import { FBXLoader } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/FBXLoader.js'
 
-//Create a Three.JS Scene
-//const scene = new THREE.Scene();
+const scene = new THREE.Scene();
 //create a new camera with positions and angles
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 10, 10000);
 
 //Keep track of the mouse position, so we can make the eye move
 let mouseX = window.innerWidth / 2;
@@ -21,54 +21,66 @@ let object;
 let controls;
 
 //Set which object to render
-let objToRender = 'door3';
+let objToRender = '2';
 
 //Instantiate a loader for the .gltf file
-let loader = new THREE.GLTFLoader();
+var loader = new GLTFLoader();
+//const textureLoader = new THREE.TextureLoader();
+//const texture = textureLoader.load("models/door6/краска2.jpg");
 
-
-var material = new THREE.MeshBasicMaterial({ color: 0xFF6913 });
+var material = new THREE.MeshPhongMaterial({ color: 0xFF6913 });
 
 //Load the file
 loader.load(
-  `static/models/${objToRender}/scene.gltf`,
-  function (gltf) {
-    //If the file is loaded, add it to the scene
-    object = gltf.scene;
-    let obj = gltf
+`static/models/${objToRender}/scene.gltf`,
+function (object) {
+//If the file is loaded, add it to the scene
+object = object.scene;
+let obj = object
 
-    obj.scene.traverse((node)=>{
-      console.log("no")
+obj.scene.traverse((node)=>{
+console.log("no")
 
-      // const glassMaterial = new THREE.MeshStandardMaterial({
-      //   opacity: 0.0,
-      // });
+// const glassMaterial = new THREE.MeshStandardMaterial({
+//   opacity: 0.0,
+// });
 
-      // glassMaterial.envMapIntensity = 1.0;
-      // glassMaterial.refractionRatio = 0.98;
+// glassMaterial.envMapIntensity = 1.0;
+// glassMaterial.refractionRatio = 0.98;
 
-      let clonedMaterial = node.material.clone();
-      node.material = clonedMaterial;
-      
-      if (node.name === "1"){
-        node.material.opacity = clonedMaterial.opacity
-        node.material,opacity = 0
-      }
-      // if (node.name === "1.1"){
-      //   console.log("success")
-      //   curMaterial.opacity = 1;
-      // }
-    })
-    scene.add(object);
-  },
-  function (xhr) {
-    //While it is loading, log the progress
-    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-  },
-  function (error) {
-    //If there is an error, log it
-    console.error(error);
-  }
+if (!node.isMesh) {
+  console.log("err")
+};
+// let clonedMaterial = node.material.clone();
+// node.material = clonedMaterial;
+
+// let secondMaterial = new THREE.MeshPhongMaterial({
+//   map: texture,
+//   opacity: 0.7,
+//   side:THREE
+//   //color: 0x00FF00,
+// })
+
+// let tempMaterial = node.material.copy()
+// tempMaterial.visible = False
+// if (node.name === "1"){
+//   node.material = tempMaterial
+// }
+// if (node.name === "1.1"){
+//   console.log("success")
+//   curMaterial.opacity = 1;
+// }
+})
+scene.add(object);
+},
+function (xhr) {
+//While it is loading, log the progress
+console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+},
+function (error) {
+//If there is an error, log it
+console.error(error);
+}
 
 
 
@@ -82,7 +94,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("container3D").appendChild(renderer.domElement);
 
 //Set how far the camera will be from the 3D model
-camera.position.z = objToRender === "door3" ? 300 : 500;
+camera.position.z = objToRender === "2" ? 3000 : 100;
 
 //Add lights to the scene, so we can actually see the 3D model
 const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
@@ -90,43 +102,43 @@ topLight.position.set(500, 500, 500) //top-left-ish
 topLight.castShadow = true;
 scene.add(topLight);
 
-const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
+const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "2" ? 5 : 1);
 scene.add(ambientLight);
 
 //This adds controls to the camera, so we can rotate / zoom it with the mouse
-if (objToRender === "door3") {
-  controls = new OrbitControls(camera, renderer.domElement);
+if (objToRender === "2") {
+controls = new OrbitControls(camera, renderer.domElement);
 }
 
 
 
 //Render the scene
 function animate() {
-  requestAnimationFrame(animate);
-  //Here we could add some code to update the scene, adding some automatic movement
+requestAnimationFrame(animate);
+//Here we could add some code to update the scene, adding some automatic movement
 
-  //Make the eye move
-  if (object && objToRender === "eye") {
-    //I've played with the constants here until it looked good 
-    object.rotation.y = -3 + mouseX / window.innerWidth * 3;
-    object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
-  }
-  renderer.render(scene, camera);
+//Make the eye move
+if (object && objToRender === "eye") {
+//I've played with the constants here until it looked good 
+object.rotation.y = -3 + mouseX / window.innerWidth * 3;
+object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
+}
+renderer.render(scene, camera);
 }
 
 
 
 //Add a listener to the window, so we can resize the window and the camera
 window.addEventListener("resize", function () {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+camera.aspect = window.innerWidth / window.innerHeight;
+camera.updateProjectionMatrix();
+renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
 //add mouse position listener, so we can make the eye move
 document.onmousemove = (e) => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
+mouseX = e.clientX;
+mouseY = e.clientY;
 }
 
 //Start the 3D rendering
